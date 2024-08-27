7a5b3b88779c402ed0957c51f66cac2b8b576c02
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-27 20:36:13 +0600

    feat: API router path operators

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -5,18 +5,21 @@ from sqlalchemy.orm import Session
 
 from blog import schemas, database, models
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/blog",
+    tags=["Blogs"],
+)
 
 get_db = database.get_db
 
 
-@router.get("/blog", response_model=List[schemas.ShowBlog], tags=["blogs"])
+@router.get("", response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@router.post("/blog", status_code=status.HTTP_201_CREATED, tags=["blogs"])
+@router.post("", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     # new_blog = models.Blog(**request.dict())
     new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
@@ -26,7 +29,7 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@router.get("/blog/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog, tags=["blogs"])
+@router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
 def show(id: int, db: Session = Depends(get_db)):
     # blog = db.query(models.Blog).get(id)
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
@@ -37,7 +40,7 @@ def show(id: int, db: Session = Depends(get_db)):
     return blog
 
 
-@router.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
 def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -47,7 +50,7 @@ def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     return "updated"
 
 
-@router.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT, tags=["blogs"])
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
