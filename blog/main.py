dcf6240856ac2bfd0d472bc6ec7f5bd838579a3a
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-27 19:26:01 +0600

    feat: Hash Password

--- blog/main.py
+++ blog/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -67,7 +68,7 @@ def destroy(id: int, db: Session = Depends(get_db)):
 
 @app.post("/user")
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-    new_user = models.User(**request.dict())
+    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
