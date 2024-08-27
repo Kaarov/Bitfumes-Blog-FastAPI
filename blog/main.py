7f89342ddae8f9508fed31c719c5b3553992cb27
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-27 17:57:31 +0600

    feat: Update a blog

--- blog/main.py
+++ blog/main.py
@@ -18,6 +18,12 @@ def get_db():
         db.close()
 
 
+@app.get("/blog")
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
 @app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     # new_blog = models.Blog(**request.dict())
@@ -26,19 +32,6 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
-
-
-@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id: int, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    return
-
-
-@app.get("/blog")
-def all(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
 
 
 @app.get("/blog/{id}", status_code=status.HTTP_200_OK)
@@ -50,3 +43,23 @@ def show(id: int, response: Response, db: Session = Depends(get_db)):
         # return {"detail": f"Blog with the id {id} does not exist"}
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} does not exist")
     return blog
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} does not exist")
+    blog.update(request.dict())
+    db.commit()
+    return "updated"
+
+
+@app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with the id {id} does not exist")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return
