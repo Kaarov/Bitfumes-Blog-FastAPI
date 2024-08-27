b690237a3210590603436296220a19ac89edd0da
Anas Kaarov <anasbinkurban@gmail.com>
2024-08-27 23:09:21 +0600

    feat: Route behind authentication

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
 from blog import schemas, database, models
@@ -13,7 +14,7 @@ router = APIRouter(
 
 
 @router.post("/login")
-def login(request: schemas.Login, db: Session = Depends(database.get_db)):
+def login(request: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
